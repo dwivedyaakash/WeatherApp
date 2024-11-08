ce04b5ccee7d781667bb21b6fea808ef719cfb95
dwivedyaakash <aakashdwd07@gmail.com>
2024-11-08 07:53:59 +0530

    daily weather forecast UI added

--- app/src/main/java/com/aakashdwivedy/weatherapp/ui/components/WeatherForecastCard.kt
+++ app/src/main/java/com/aakashdwivedy/weatherapp/ui/components/WeatherForecastCard.kt
@@ -1,15 +1,88 @@
 package com.aakashdwivedy.weatherapp.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aakashdwivedy.weatherapp.R
 import com.aakashdwivedy.weatherapp.model.forecast.response.ForecastResponse
+import com.aakashdwivedy.weatherapp.utils.getFormattedDate
 
 @Composable
 fun WeatherForecastCard(forecastData: ForecastResponse) {
-    Text(forecastData.timezone)
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .background(Color.White, shape = RoundedCornerShape(16.dp))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 8.dp, start = 8.dp, bottom = 8.dp),
+            text = "${forecastData.daily.time.size} days forecast",
+            fontSize = 18.sp
+        )
+        Column(Modifier.fillMaxWidth()) {
+            forecastData.daily.time.indices.toList().forEach {
+                WeatherForecastCardItem(
+                    forecastData.daily.time[it],
+                    forecastData.daily.temperature_2m_max[it],
+                    forecastData.daily.temperature_2m_min[it],
+                    forecastData.daily_units.temperature_2m_max
+                )
+            }
+        }
+    }
     Spacer(Modifier.height(16.dp))
+}
+
+@Composable
+fun WeatherForecastCardItem(
+    date: String, tempMax: Double,
+    tempMin: Double, unit: String
+) {
+    val formattedDate = getFormattedDate(date)
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color(0xFFC6E7FF), shape = RoundedCornerShape(8.dp))
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(0.4f),
+            text = formattedDate,
+            fontWeight = FontWeight.SemiBold
+        )
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "$tempMin$unit")
+            Image(
+                modifier = Modifier.size(30.dp),
+                painter = painterResource(R.drawable.sunrise),
+                contentDescription = "image"
+            )
+            Text(text = "$tempMax$unit")
+        }
+    }
 }
--- app/src/main/java/com/aakashdwivedy/weatherapp/utils/Utils.kt
+++ app/src/main/java/com/aakashdwivedy/weatherapp/utils/Utils.kt
@@ -1,8 +1,10 @@
 package com.aakashdwivedy.weatherapp.utils
 
 import java.time.Instant
+import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 fun getTempInCelsius(temp: Double): String {
     return "${(temp - 273.15).toString().substringBefore(".")}°C"
@@ -13,4 +15,16 @@ fun getFormattedTime(timestamp: Int): String {
     val formatter = DateTimeFormatter.ofPattern("HH:mm a")
         .withZone(ZoneId.systemDefault())
     return formatter.format(instant)
+}
+
+fun getFormattedDate(dateString: String): String {
+    val date = LocalDate.parse(dateString)
+    val today = LocalDate.now()
+    val tomorrow = today.plusDays(1)
+
+    return when {
+        date.isEqual(today) -> "Today"
+        date.isEqual(tomorrow) -> "Tomorrow"
+        else -> date.dayOfWeek.getDisplayName(java.time.format.TextStyle.FULL, Locale.getDefault())
+    }
 }
